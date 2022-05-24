98b9e4ec2ac45c57e494ad47fa70c5d5bf01940a
Nhu <qnhu96@gmail.com>
2022-05-24 17:48:43 +0700

    Implement socket for add friend feature

--- src/gateways/app.gateway.ts
+++ src/gateways/app.gateway.ts
@@ -4,7 +4,6 @@ import {
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer,
-    WsResponse,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import {
@@ -14,15 +13,27 @@ import {
     CommentNotifyResponseInterface,
     ReactNotifyRequestInterface,
     LikePostMessageResponseInterface,
-    LikePostNotificationResponseInterface
+    LikePostNotificationResponseInterface,
+    AddFriendNotifyRequestInterface,
+    AddFriendMessageResponseInterface,
+    AddFriendNotifyResponseInterface,
+    CancelFriendRequestNotifyRequestInterface,
+    CancelFriendRequestNotifyResponseInterface,
+    DeleteFriendRequestNotifyRequestInterface,
+    DeleteFriendRequestNotifyResponseInterface,
+    AcceptFriendNotifyRequestInterface,
+    AcceptFriendNotifyResponseInterface,
+    AcceptFriendMessageResponseInterface
 } from './interfaces/messages.interface';
 import { JwtService } from '@nestjs/jwt';
-import { IDeviceRepository } from '../core/repositories/idevice.repository';
-import { REPOSITORY_INTERFACE } from '../core/config/module.config';
-import { DeviceEntity } from '../core/entities/device.entity';
-import { IPostRepository } from '../core/repositories/ipost.repository';
-import { IPostLikedUsersRepository } from '../core/repositories/ipost-liked-users.repository';
-import { ORDER_BY } from '../core/common/constants/common.constant';
+import { IDeviceRepository } from 'src/core/repositories/idevice.repository';
+import { REPOSITORY_INTERFACE, SERVICE_INTERFACE } from 'src/core/config/module.config';
+import { DeviceEntity } from 'src/core/entities/device.entity';
+import { IPostRepository } from 'src/core/repositories/ipost.repository';
+import { IPostLikedUsersRepository } from 'src/core/repositories/ipost-liked-users.repository';
+import { ORDER_BY } from 'src/core/common/constants/common.constant';
+import { IProfileService } from 'src/core/services/iprofile.service';
+import databaseConfig from 'src/core/config/database.config';
 const options = {
     cors: {
         origin: ["http://localhost:3000", "example2.com"],
@@ -41,7 +52,8 @@ export class AppGateway
         private jwtService: JwtService,
         @Inject(REPOSITORY_INTERFACE.IDEVICE_REPOSITORY) private _deviceRepos: IDeviceRepository,
         @Inject(REPOSITORY_INTERFACE.IPOST_REPOSITORY) private _postRepos: IPostRepository,
-        @Inject(REPOSITORY_INTERFACE.IPOST_LIKED_USERS_REPOSITORY) private _postLikedUsersRepos: IPostLikedUsersRepository
+        @Inject(REPOSITORY_INTERFACE.IPOST_LIKED_USERS_REPOSITORY) private _postLikedUsersRepos: IPostLikedUsersRepository,
+        @Inject(SERVICE_INTERFACE.IPROFILE_SERVICE) private _profileService: IProfileService
     ) { }
 
     // afterInit(server: any): any {
@@ -93,8 +105,8 @@ export class AppGateway
 
     @SubscribeMessage('client-add-comment')
     async addCommentNotification(@MessageBody() payload: CommentNotifyRequestInterface): Promise<any> {
-        const post = await this._postRepos.findOne({ id: payload.data.postId, isDeleted: false });
-        const device = await this._deviceRepos.findOne({ userId: post.userId, isDeleted: false });
+        const Post = await this._postRepos.findOne({ id: payload.data.postId, isDeleted: false });
+        const device = await this._deviceRepos.findOne({ userId: Post.userId, isDeleted: false });
         const data: CommentNotifyResponseInterface = {
             postId: payload.data.postId,
             fromUserName: payload.data.fromUserName,
@@ -103,15 +115,15 @@ export class AppGateway
         this._logger.log('client-add-comment: emit message, type: ADD_COMMENT_MESSAGE_FROM_SERVER');
         this.server.emit('message', { type: 'ADD_COMMENT_MESSAGE_FROM_SERVER', data });
         if (device) {
-            this._logger.log('client-add-comment: emit message, type: ADD_COMMENT_NOTIFICATION_FROM_SERVER');
-            this.server.to(device.socketId).emit('message', { type: 'ADD_COMMENT_NOTIFICATION_FROM_SERVER', data });
+            this._logger.log('client-add-comment: emit message, type: ADD_COMMENT_NOTIFICATION_MESSAGE_FROM_SERVER');
+            this.server.to(device.socketId).emit('message', { type: 'ADD_COMMENT_NOTIFICATION_MESSAGE_FROM_SERVER', data });
         }
     }
 
     @SubscribeMessage('client-update-comment')
     async updateCommentNotification(@MessageBody() payload: CommentNotifyRequestInterface): Promise<any> {
-        const post = await this._postRepos.findOne({ id: payload.data.postId, isDeleted: false });
-        const device = await this._deviceRepos.findOne({ userId: post.userId, isDeleted: false });
+        const Post = await this._postRepos.findOne({ id: payload.data.postId, isDeleted: false });
+        const device = await this._deviceRepos.findOne({ userId: Post.userId, isDeleted: false });
         const data: CommentNotifyResponseInterface = {
             postId: payload.data.postId,
             fromUserName: payload.data.fromUserName,
@@ -124,16 +136,14 @@ export class AppGateway
     @SubscribeMessage('client-like-post')
     async reactNotification(@MessageBody() payload: ReactNotifyRequestInterface): Promise<any> {
         this._logger.log('client-like-post: payload, type: LIKE_POST_MESSAGE_FROM_SERVER', payload);
-        const post = await this._postRepos.findOne({ id: payload.data.postId, isDeleted: false });
-        const device = await this._deviceRepos.findOne({ userId: post.userId, isDeleted: false });
+        const Post = await this._postRepos.findOne({ id: payload.data.postId, isDeleted: false });
+        const device = await this._deviceRepos.findOne({ userId: Post.userId, isDeleted: false });
         const result = await this._postLikedUsersRepos.findByCondition(
             {
                 postId: payload.data.postId,
                 isDeleted: false
             },
-            { createdAt: ORDER_BY.DESC }
-        );
-
+            { createdAt: ORDER_BY.DESC });
         const data: LikePostMessageResponseInterface = {
             postId: payload.data.postId,
             likes: result.length,
@@ -143,7 +153,6 @@ export class AppGateway
         }
         this._logger.log('client-like-post: emit message, type: LIKE_POST_MESSAGE_FROM_SERVER');
         this.server.emit('message', { type: 'LIKE_POST_MESSAGE_FROM_SERVER', data });
-        
         if (device) {
             const data: LikePostNotificationResponseInterface = {
                 like: payload.data.like,
@@ -151,8 +160,102 @@ export class AppGateway
                 fromUserName: payload.data.fromUserName,
                 fromUserId: payload.data.fromUserId
             }
-            this._logger.log('client-like-post: emit message, type: LIKE_POST_NOTIFICATION_FROM_SERVER');
-            this.server.to(device.socketId).emit('message', { type: 'LIKE_POST_NOTIFICATION_FROM_SERVER', data });
+            this._logger.log('client-like-post: emit message, type: LIKE_POST_NOTIFICATION_MESSAGE_FROM_SERVER');
+            this.server.to(device.socketId).emit('message', { type: 'LIKE_POST_NOTIFICATION_MESSAGE_FROM_SERVER', data });
         }
+    }
+
+    @SubscribeMessage('client-add-friend')
+    async addFriendNotification(@MessageBody() payload: AddFriendNotifyRequestInterface): Promise<any> {
+        this._logger.log('client-add-friend: payload, type: ADD_FRIEND_SOCKET_HANDLER', payload);
+        const device = await this._deviceRepos.findOne({ userId: payload?.receiverId, isDeleted: false });
+        const senderId = payload?.senderId;
+        const receiverId = payload?.receiverId;
+        const socketId = device?.socketId;
+        this.emitAddFriendMessage(senderId, receiverId, socketId);
+        await this.emitAddFriendNotification(senderId, receiverId, socketId);
+    }
+
+    @SubscribeMessage('client-accept-friend')
+    async acceptFriendNotification(@MessageBody() payload: AcceptFriendNotifyRequestInterface): Promise<any> {
+        this._logger.log('client-accept-friend: payload, type: ACCEPT_FRIEND_SOCKET_HANDLER', payload);
+        const device = await this._deviceRepos.findOne({ userId: payload?.senderId, isDeleted: false });
+        const senderId = payload?.senderId;
+        const receiverId = payload?.receiverId;
+        const socketId = device?.socketId;
+        this.emitAcceptFriendMessage(senderId, receiverId, socketId);
+        await this.emitAcceptFriendNotification(senderId, receiverId, socketId);
+    }
+
+    @SubscribeMessage('client-cancel-friend-request')
+    async cancelFriendRequestNotification(@MessageBody() payload: CancelFriendRequestNotifyRequestInterface): Promise<any> {
+        this._logger.log('client-cancel-friend-request: payload, type: CANCEL_FRIEND_REQUEST_SOCKET_HANDLER', payload);
+        const device = await this._deviceRepos.findOne({ userId: payload?.receiverId, isDeleted: false });
+        const senderId = payload?.senderId;
+        const receiverId = payload?.receiverId;
+        const socketId = device?.socketId;
+        const data: CancelFriendRequestNotifyResponseInterface = {
+            senderId: senderId,
+            receiverId: receiverId
+        }
+        this._logger.log('client-cancel-friend-request: emit message, type: CANCEL_FRIEND_REQUEST_MESSAGE_FROM_SERVER');
+        this.server.to(socketId).emit('message', { type: 'CANCEL_FRIEND_REQUEST_MESSAGE_FROM_SERVER', data });
+    }
+
+    @SubscribeMessage('client-delete-friend-request')
+    async deleteFriendRequestNotification(@MessageBody() payload: DeleteFriendRequestNotifyRequestInterface): Promise<any> {
+        this._logger.log('client-delete-friend-request: payload, type: DELETE_FRIEND_REQUEST_SOCKET_HANDLER', payload);
+        const device = await this._deviceRepos.findOne({ userId: payload?.senderId, isDeleted: false });
+        const senderId = payload?.senderId;
+        const receiverId = payload?.receiverId;
+        const socketId = device?.socketId;
+        const data: DeleteFriendRequestNotifyResponseInterface = {
+            senderId: senderId,
+            receiverId: receiverId
+        }
+        this._logger.log('client-delete-friend-request: emit message, type: DELETE_FRIEND_REQUEST_MESSAGE_FROM_SERVER');
+        this.server.to(socketId).emit('message', { type: 'DELETE_FRIEND_REQUEST_MESSAGE_FROM_SERVER', data });
+    }
+
+    emitAddFriendMessage(senderId: number, receiverId: number, socketId: string) {
+        const data: AddFriendMessageResponseInterface = {
+            senderId: senderId,
+            receiverId: receiverId
+        }
+        this._logger.log('client-add-friend: emit message, type: ADD_FRIEND_MESSAGE_FROM_SERVER');
+        this.server.to(socketId).emit('message', { type: 'ADD_FRIEND_MESSAGE_FROM_SERVER', data });
+    }
+
+    async emitAddFriendNotification(senderId: number, receiverId: number, socketId: string) {
+        const profile = await this._profileService.getProfileDetailByUserId(senderId);
+        const data: AddFriendNotifyResponseInterface = {
+            senderId: senderId,
+            receiverId: receiverId,
+            senderName: `${profile.Data.firstName} ${profile.Data.lastName}`,
+            senderAvatar: profile.Data.avatar,
+        }
+        this._logger.log('client-add-friend: emit message, type: ADD_FRIEND_NOTIFICATION_MESSAGE_FROM_SERVER');
+        this.server.to(socketId).emit('message', { type: 'ADD_FRIEND_NOTIFICATION_MESSAGE_FROM_SERVER', data });
+    }
+
+    emitAcceptFriendMessage(senderId: number, receiverId: number, socketId: string) {
+        const data: AcceptFriendMessageResponseInterface = {
+            senderId: senderId,
+            receiverId: receiverId
+        }
+        this._logger.log('client-accept-friend: emit message, type: ACCEPT_FRIEND_MESSAGE_FROM_SERVER');
+        this.server.to(socketId).emit('message', { type: 'ACCEPT_FRIEND_MESSAGE_FROM_SERVER', data });
+    }
+
+    async emitAcceptFriendNotification(senderId: number, receiverId: number, socketId: string) {
+        const profile = await this._profileService.getProfileDetailByUserId(receiverId);
+        const data: AcceptFriendNotifyResponseInterface = {
+            senderId: senderId,
+            receiverId: receiverId,
+            receiverAvatar: profile.Data.avatar,
+            receiverName: `${profile.Data.firstName} ${profile.Data.lastName}`
+        }
+        this._logger.log('client-accept-friend: emit message, type: ACCEPT_FRIEND_NOTIFICATION_MESSAGE_FROM_SERVER');
+        this.server.to(socketId).emit('message', { type: 'ACCEPT_FRIEND_NOTIFICATION_MESSAGE_FROM_SERVER', data });
     }
 }
--- src/gateways/interfaces/messages.interface.ts
+++ src/gateways/interfaces/messages.interface.ts
@@ -1,5 +1,9 @@
 import { bool } from "aws-sdk/clients/signer"
 
+interface FriendRequestBaseInterface {
+    senderId: number,
+    receiverId: number
+}
 export interface MessagesRequestInterface {
     data: {
         message: string,
@@ -38,6 +42,22 @@ export interface ReactNotifyRequestInterface {
     }
 }
 
+export interface AddFriendNotifyRequestInterface extends FriendRequestBaseInterface {
+
+}
+
+export interface AcceptFriendNotifyRequestInterface extends FriendRequestBaseInterface {
+
+}
+
+export interface CancelFriendRequestNotifyRequestInterface extends FriendRequestBaseInterface {
+
+}
+
+export interface DeleteFriendRequestNotifyRequestInterface extends FriendRequestBaseInterface {
+
+}
+
 export interface LikePostMessageResponseInterface {
     postId: number,
     likes: number,
@@ -51,4 +71,29 @@ export interface LikePostNotificationResponseInterface {
     like: bool,
     fromUserId: number,
     fromUserName: string
+}
+
+export interface AddFriendMessageResponseInterface extends FriendRequestBaseInterface {
+
+}
+
+export interface AddFriendNotifyResponseInterface extends FriendRequestBaseInterface {
+    senderName: string,
+    senderAvatar: string
+}
+
+export interface AcceptFriendMessageResponseInterface extends FriendRequestBaseInterface {
+
+}
+
+export interface AcceptFriendNotifyResponseInterface extends FriendRequestBaseInterface {
+    receiverName: string,
+    receiverAvatar: string
+}
+
+export interface CancelFriendRequestNotifyResponseInterface extends FriendRequestBaseInterface {
+
+}
+export interface DeleteFriendRequestNotifyResponseInterface extends FriendRequestBaseInterface {
+
 }
